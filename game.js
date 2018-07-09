8b86291e165de462288c500890f2233a9cae80f8
Jon Zachary <jontzachary@gmail.com>
2018-07-09 01:54:20 -0400

    fix bet total bug, pull out checkPoint fn

--- game.js
+++ game.js
@@ -17,19 +17,18 @@ const player = (name, bankroll) => ({ name, bankroll: [bankroll] });
 
 const winLose = (aPlayer, amt) => aPlayer.bankroll.push(amt);
 
-const totalBankroll = (aPlayer, bet) => aPlayer.bankroll.reduce((a, b) => a + b);
+const totalBankroll = (aPlayer, bet = 0) => aPlayer.bankroll.reduce((a, b) => a + b) - bet;
 
 const checkComeOut = (aRoll, aPlayer, bet) => {
   let comeOutFlag = null;
   let newPoint = null;
-  let betAmt = bet;
+  const betAmt = bet;
   switch (total(aRoll)) {
     case 7:
     case 11:
       console.log('Pay the line!\n');
       winLose(aPlayer, bet);
       comeOutFlag = true;
-      betAmt = 0;
       break;
     case 2:
     case 3:
@@ -37,7 +36,6 @@ const checkComeOut = (aRoll, aPlayer, bet) => {
       console.log('Craps!\n');
       winLose(aPlayer, (-1 * bet));
       comeOutFlag = true;
-      betAmt = 0;
       break;
     default:
       newPoint = total(aRoll);
@@ -48,33 +46,43 @@ const checkComeOut = (aRoll, aPlayer, bet) => {
 };
 
 const msg = (aGame, aRoll, aBet) => {
-  const bnk = 
+  let res = aBet;
+  if (!aGame.newPoint) res = 0;
   console.log(`${aGame.aPlayer.name}, you have rolled ${aRoll} and wagered ${aBet}
-your point is ${aGame.newPoint || 'Off'} and your bankroll is ${totalBankroll(aGame.aPlayer, aBet)}\n`);
+your point is ${aGame.newPoint || 'Off'} and your bankroll is ${totalBankroll(aGame.aPlayer, res)}\n`);
 };
 
 const comeOutRoll = (aGame) => {
   const newRoll = roll(2);
   const res = checkComeOut(newRoll, aGame.aPlayer, aGame.bet);
-  msg(res, newRoll, aGame.bet);
+  msg(res, newRoll, res.betAmt);
   return game(aGame.aPlayer, res.comeOutFlag, res.newPoint, aGame.bet);
+};
+
+const checkPoint = (aGame, aRoll) => {
+  const myGame = aGame;
+  const rollTotal = total(aRoll);
+  switch (rollTotal) {
+    case myGame.point:
+      winLose(myGame.aPlayer, myGame.bet);
+      console.log(`you rolled ${aRoll}! Winner winner chicken dinner! your bankroll is ${totalBankroll(myGame.aPlayer)}\n`);
+      myGame.comeOut = true;
+      break;
+    case 7:
+      winLose(myGame.aPlayer, (-1 * myGame.bet));
+      console.log(`you rolled ${aRoll} Seven out! your bankroll is ${totalBankroll(myGame.aPlayer)}\n`);
+      myGame.comeOut = true;
+      break;
+    default:
+      console.log(`${myGame.aPlayer.name}, you have rolled ${aRoll} your point is ${myGame.point}\n`);
+  }
+  return myGame;
 };
 
 const pointRoll = (aGame) => {
   const newRoll = roll(2);
-  let comeOutFlag = false;
-  if (total(newRoll) === aGame.point) {
-    winLose(aGame.aPlayer, aGame.bet);
-    console.log(`you rolled ${newRoll}! Winner winner chicken dinner! your bankroll is ${totalBankroll(aGame.aPlayer, aGame.bet)}\n`);
-    comeOutFlag = true;
-  } else if (total(newRoll) === 7) {
-    winLose(aGame.aPlayer, (-1 * aGame.bet));
-    console.log(`you rolled ${newRoll} Seven out! your bankroll is ${totalBankroll(aGame.aPlayer, 0)}\n`);
-    comeOutFlag = true;
-  } else {
-    console.log(`${aGame.aPlayer.name}, you have rolled ${newRoll} your point is ${aGame.point}\n`);
-  }
-  return game(aGame.aPlayer, comeOutFlag, aGame.point, aGame.bet);
+  const res = checkPoint(aGame, newRoll);
+  return game(res.aPlayer, res.comeOut, res.point, res.bet);
 };
 
 const jon = player('jon', 100);
